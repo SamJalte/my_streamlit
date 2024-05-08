2b6c2b639d9f9a0943c21935e04317f6ef0e4da8
SamJalte <161337521+SamJalte@users.noreply.github.com>
2024-05-08 20:26:20 +0200

    Add files via upload

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -1,46 +1,64 @@
+# Imortant pour l'utilisation de streamlit : il faut ouvrir le terminal sur mac 
+#1. Lancer : cd /Users/jalte/Desktop/WCS/test_streamlit
+#2. streamlit run my_streamlit_app.py
 import streamlit as st
 import pandas as pd
-
-''' Imortant pour l'utilisation de streamlit : il faut ouvrir le terminal sur mac 
-1. Lancer : cd /Users/jalte/Desktop/WCS/test_streamlit
-2. streamlit run my_streamlit_app.py
-'''
-
-st.title('Hello Wilders, welcome to my application!')
-name = st.text_input("Please give me your name :")
-name_length = len(name)
-st.write("Your name has ",name_length,"characters")
-
-
-
-st.write("I enjoy to discover stremalit possibilities")
-
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
-df_weather = pd.read_csv(link)
-st.write(df_weather)
-
-# Here we use "magic commands":
-df_weather
-
-#Identifier les colonnes non-numériques
-df_weather.dtypes   
-
-# Exclure les colonnes non-numériques
-import numpy as np
-df_numeric = df_weather.select_dtypes(include=[np.number])
-#Calculer la matrice de corrélation
-correlation_matrix = df_numeric.corr()
-
-
-st.line_chart(df_weather['MAX_TEMPERATURE_C'])
-
-# Utiliser Seaborn pour la heatmap
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
-viz_correlation = sns.heatmap(correlation_matrix, center=0, cmap=sns.color_palette("vlag", as_cmap=True))
-plt.show()
+st.title('Application qui analyse et visualise les données des voitures !')
+
+st.write("Chargement des données")
+
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df_voiture = pd.read_csv(link)
+
+#df_voiture
+
+#df_voiture.dtypes   
+
+# Convertir la colonne 'continent' pour une meilleurs lecture
+region_mapping = {' US.': 'US', ' Europe.': 'Europe', ' Japan.': 'Japan'}
+df_voiture['continent'] = df_voiture['continent'].map(region_mapping)
+
+df_voiture
+
+# Sélection de la région
+
+region = st.sidebar.multiselect('Selection des regions:', options=df_voiture['continent'].unique(), default=df_voiture['continent'].unique())
+
+st.write("Filtrage des données selon la sélection de la région")
+
+filtered_data = df_voiture[df_voiture['continent'].isin(region)]
+
+filtered_data
+
+st.write("Visualisation des données")
+
+st.subheader('Histogramme de la distribution des mpg')
+fig, ax = plt.subplots()
+sns.histplot(filtered_data['mpg'], kde=True, ax=ax)
+st.pyplot(fig)
+
+st.subheader('Diagramme de la corrélation')
+
+# Convertir 'continent' en valeurs numériques catégorielles
+filtered_data['continent'] = filtered_data['continent'].astype('category').cat.codes
+
+# Calcul de la matrice de corrélation
+corr_matrix = filtered_data.corr()
+# Création du heatmap
+plt.figure(figsize=(10, 8))
+viz_correlation = sns.heatmap(corr_matrix,
+                              center=0,
+                              cmap=sns.color_palette('vlag',as_cmap=True),
+)
 st.pyplot(viz_correlation.figure)
+
+
+
+
 
 
 
